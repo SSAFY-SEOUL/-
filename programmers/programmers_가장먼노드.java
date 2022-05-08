f241ab4c9f3395ca19d41767a699362812f3e37b
jekwan <jekwan@kakao.com>
2022-05-08 20:45:18 +0900

    Upload Solution

---
+++ programmers/programmers_가장먼노드.java
@@ -0,0 +1,65 @@
+package com.ssafy.programmers;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+	
+	public static void main(String[] args) {
+		int n = 6;
+		int[][] edge = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+		
+		System.out.println(solution(n, edge));
+	}
+	
+	private static class State {
+		int node, step;
+
+		public State(int node, int step) {
+			this.node = node;
+			this.step = step;
+		}
+	}
+	
+    public static int solution(int n, int[][] edge) {
+        int answer = 0;
+        
+        List<Integer>[] adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; ++i) adj[i] = new ArrayList<>();
+        
+        for (int i = 0, cnt = edge.length; i < cnt; ++i) {
+        	adj[edge[i][0]].add(edge[i][1]);
+        	adj[edge[i][1]].add(edge[i][0]);
+        }
+        
+        
+        boolean[] visit = new boolean[n + 1];
+        Queue<State> q = new LinkedList<>();
+        q.add(new State(1, 0));
+        visit[1] = true;
+        
+        State cur;
+        int maxDistance = 0;
+        answer = 0;
+        while (!q.isEmpty()) {
+        	cur = q.poll();
+        	
+        	if (cur.step == maxDistance) answer++;
+        	else if (cur.step > maxDistance) {
+        		maxDistance = cur.step;
+        		answer = 1;
+        	}
+        	
+        	for (int next : adj[cur.node]) {
+        		if (!visit[next]) {
+        			q.add(new State(next, cur.step + 1));
+        			visit[next] = true;
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+}

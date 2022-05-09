13919ec5cc6cb36fbc8f5ebd13ff867234208d41
jekwan <jekwan@kakao.com>
2022-05-09 23:36:02 +0900

    Upload Solution

---
+++ boj/boj_2056_DP.java
@@ -0,0 +1,32 @@
+package com.ssafy.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[] end = new int[N + 1];
+		int[] time = new int[N + 1];
+		
+		int max = 0, need = 0;
+		for (int i = 1; i <= N; ++i) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			time[i] = Integer.parseInt(st.nextToken());
+			need = Integer.parseInt(st.nextToken());
+			
+			end[i] = time[i];
+			for (int j = 0; j < need; ++j) {
+				int before = Integer.parseInt(st.nextToken());
+				end[i] = Math.max(end[i], end[before] + time[i]);
+			}
+			
+			max = Math.max(max, end[i]);
+		}
+		
+		System.out.println(max);
+	}
+}
